aa947db26b80d64359955f0a37c80747f82c51e5
PedroGuevara0527 <pjg752000@gmail.com>
2025-11-12 10:55:42 -0500

    modified calculator p2

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+import math
 """
 calculator.py
 - Defines functions used to create a simple calculator
@@ -5,7 +6,30 @@ calculator.py
 One function per operation, in order.
 """
 # First example
-def add(a, b): 
-    pass
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    a - b
+
+def mul(a, b):
+    a * b
+
+# Divides b by a (b/a)
+def div(a, b):
+    try:
+        return b/a
+    except ZeroDivisionError:
+        return("Can't divide by zero!")
+
+def log(a, b):
+    try:
+        math.log(a, b)
+    except ValueError:
+        print("Can't take the logorithm of a negative number!")
+
+def exp(a, b):
+    return math.pow(a, b)
+
 
 
